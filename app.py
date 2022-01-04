5c1d4216c285accad7fe378362a60d847c63ce08
Rogelio García <38191792+rgarciajim@users.noreply.github.com>
2022-01-04 23:12:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ def get_visit_count():
 @app.route('/')
 def show_visit_count():
     visit_count = get_visit_count()
-    str='#### App visitor count Docker v.0.0.1 #####<br/>This is the {} visitor.\n'.format(visit_count)
+    str='This is the {} visitor.\n'.format(visit_count)
     return str
     
 @app.route('/version')
 def show_version():
-    str='#### App visitor count Docker v.0.0.2 #####'
+    str='#### VERSION App visitor count Docker v.0.0.2 #########<br/>This is the {} visitor.\n'.format(visit_count)
     return str
     
